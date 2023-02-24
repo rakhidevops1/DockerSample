bc077a699f7a9a1c4a90676dfd93ada689868f18
Ubuntu <ubuntu@ip-172-31-34-236.ap-south-1.compute.internal>
2023-02-24 17:51:02 +0000

    All Sample of Docker

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM openjdk:18-ea-11-jdk-alpine3.15
+RUN apk update  && apk add git 
---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pwd
+echo "USERNAME:" $(whoami)
+date
+ls -lar
+env
+echo "ENV DEMO $PROJECT_HOME"
